e434f00eddc4d110a96237ccee4fefdb2d553a22
Mina <stanton.mina@gmail.com>
2020-02-23 22:59:52 -0500

    First commit MVC lab

---
+++ MVC_Lab/Controllers/TripController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Lab.Models;
+
+namespace MVC_Lab.Controllers
+{
+    public class TripController : Controller
+    {
+        // GET: Default
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult MileageForm()
+        {
+            return View();
+        }
+
+       
+        public IActionResult MileageResult(Tripometer trips)
+        {
+            return View(trips);
+        }
+    }
+}
