3a43584d2b1bd98b249c858086dddec85da177dc
Mina <stanton.mina@gmail.com>
2020-02-25 09:12:58 -0500

    Second commit added two methods to add numbers and divide numbers

---
+++ MVC_Lab/Models/Maths.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Lab.Models
+{
+    public class Maths
+    {
+
+
+
+        public static string Add(int num1, int num2)
+        {
+            int total = num1 + num2;
+            return total.ToString();
+        }
+
+        public static string Divide(int num1, int num2)
+        {
+            double quotient = num1 / num2;
+            double remainder = num1 % num2;
+
+            return $"{quotient} with a remainder of {remainder}";
+        }
+    }
+}
